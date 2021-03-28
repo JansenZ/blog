43b92f7bfda7dc83146add81a09919684d012dc4
Jansen <zhangzhenglin00@163.com>
2021-03-28 09:29:35 +0800

    Update promise.js

--- docs/js/promise.js
+++ docs/js/promise.js
@@ -147,6 +147,13 @@ class Promise2 {
 }
 
 
+Promise.prototype.finally = function (callback) {
+  let P = this.constructor;
+  return this.then(
+    value  => P.resolve(callback()).then(() => value),
+    reason => P.resolve(callback()).then(() => { throw reason })
+  );
+};
 
 
 
