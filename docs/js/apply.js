a80e12616711b11cccbffdb583c954cc911a3ac9
zhangzhenglin <zhangzhenglin@meituan.com>
2026-05-15 15:57:40 +0800

    fix: apply实现补上展开运算符

--- docs/js/apply.js
+++ docs/js/apply.js
@@ -1,10 +1,10 @@
-// apply 返回函数结果
+// apply 接收一个数组参数，返回函数结果
 Function.prototype.apply2 = function(context, args) {
     let fn = Symbol("fn");
-    context = context || window;
+    context = context == null ? window : Object(context);
     context[fn] = this;
 
-    let res = context[fn](args);
+    let res = context[fn](...args);
 
     delete context[fn];
 
